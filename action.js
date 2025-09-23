b875616864c09ee108c9fc74ad32e37109808a9c
Antonio Vilarta <avilgil98@gmail.com>
2025-09-23 23:44:52 +0200

    Añadida casi toda la lógica en JS, falta corregir porcentaje y añadir opción a decimales

--- action.js
+++ action.js
@@ -1,4 +1,3 @@
-
 const cajaDisplay = document.querySelector(".display");
 const buttonAC = document.getElementById("buttonAC");
 const buttonDEL = document.getElementById("buttonDEL");
@@ -19,43 +18,117 @@ const button8 = document.getElementById("button8");
 const button9 = document.getElementById("button9");
 const button0 = document.getElementById("button0");
 const buttonCom = document.getElementById("buttonCom");
+var num1 = "";
+var num2 = "";
+var resultado = 0;
+var comprobante = false;
+var operando = "";
 
-
-cambiarDisplay();
-
-function cambiarDisplay(){
-    buttonAC.addEventListener("click", function(){
-        cajaDisplay.textContent = "0";
-    })
-    button0.addEventListener("click", function(){
-        cajaDisplay.textContent = "0";
-    })
-    button1.addEventListener("click", function(){
-        cajaDisplay.textContent = "1";
-    })
-    button2.addEventListener("click", function(){
-        cajaDisplay.textContent = "2";
-    })
-    button3.addEventListener("click", function(){
-        cajaDisplay.textContent = "3";
-    })
-    button4.addEventListener("click", function(){
-        cajaDisplay.textContent = "4";
-    })
-    button5.addEventListener("click", function(){
-        cajaDisplay.textContent = "5";
-    })
-    button6.addEventListener("click", function(){
-        cajaDisplay.textContent = "6";
-    })
-    button7.addEventListener("click", function(){
-        cajaDisplay.textContent = "7";
-    })
-    button8.addEventListener("click", function(){
-        cajaDisplay.textContent = "8";
-    })
-    button9.addEventListener("click", function(){
-        cajaDisplay.textContent = "9";
-    })
-    
+function actualizarDisplay() {
+  comprobante === false
+    ? (cajaDisplay.textContent = num1 || "0")
+    : (cajaDisplay.textContent = num2 || "0");
 }
+
+function addNumero(btn) {
+  btn.addEventListener("click", function () {
+    comprobante === false
+      ? (num1 += btn.textContent)
+      : (num2 += btn.textContent);
+    actualizarDisplay();
+  });
+}
+
+buttonDEL.addEventListener("click", function () {
+  comprobante == false
+    ? (num1 = num1.slice(0, -1))
+    : (num2 = num2.slice(0, -1));
+  actualizarDisplay();
+});
+buttonAC.addEventListener("click", function () {
+  num1 = "";
+  num2 = "";
+  actualizarDisplay();
+});
+
+buttonMas.addEventListener("click", function () {
+  comprobante = true;
+  operando = "+";
+  actualizarDisplay();
+});
+buttonMen.addEventListener("click", function () {
+  comprobante = true;
+  operando = "-";
+  actualizarDisplay();
+});
+buttonDiv.addEventListener("click", function () {
+  comprobante = true;
+  operando = "/";
+  actualizarDisplay();
+});
+buttonMul.addEventListener("click", function () {
+  comprobante = true;
+  operando = "*";
+  actualizarDisplay();
+});
+buttonPor.addEventListener("click", function () {
+  comprobante = true;
+  operando = "%";
+  actualizarDisplay();
+});
+
+buttonIgu.addEventListener("click", function () {
+  comprobante = false;
+  switch (operando) {
+    case "+":
+      resultado = Number(num1) + Number(num2);
+      cajaDisplay.textContent = resultado;
+      num1 = "";
+      num2 = "";
+      comprobante = false;
+      break;
+    case "-":
+      resultado = Number(num1) - Number(num2);
+      cajaDisplay.textContent = resultado;
+      num1 = "";
+      num2 = "";
+      comprobante = false;
+      break;
+    case "/":
+      resultado = Number(num1) / Number(num2);
+      cajaDisplay.textContent = resultado;
+      num1 = "";
+      num2 = "";
+      comprobante = false;
+      break;
+    case "*":
+      resultado = Number(num1) * Number(num2);
+      cajaDisplay.textContent = resultado;
+      num1 = "";
+      num2 = "";
+      comprobante = false;
+      break;
+    case "%":
+      resultado = Number(num1) % Number(num2);
+      cajaDisplay.textContent = resultado;
+      num1 = "";
+      num2 = "";
+      comprobante = false;
+      break;
+  }
+});
+
+[
+  button0,
+  button1,
+  button2,
+  button3,
+  button4,
+  button5,
+  button6,
+  button7,
+  button8,
+  button9,
+].forEach(addNumero);
+
+actualizarDisplay();
